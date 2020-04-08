b727605d55a6f047c50d845bd43e7227cb8d6bf3
PamelaLucio <61605354+PamelaLucio@users.noreply.github.com>
2020-04-07 22:27:54 -0300

    commit on master

--- aula0704/aula07-04 - 2.py
+++ aula0704/aula07-04 - 2.py
@@ -1,9 +1,10 @@
+#Exercio 1#
 class Funcionario:
 
     def __init__(self, nome, cpf, salario):
-        self.nome = nome
-        self.cpf = cpf
-        self.salario = salario
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__salario = salario
     
     def get_nome(self):
         return self.__nome
@@ -26,10 +27,12 @@ class Funcionario:
     def set_salario(self, salario):
         self.__salario = salario
     
-func1 = Funcionario("Pedro", "111222333-22", 1500.0)
+func1 = Funcionario("Pedro", "", 1500.0)
 func1.salario = 3000.0              
-print("Nome:", func1.nome)
-print("CPF:", func1.cpf)
-print("Salário:", func1.salario)
+print("Nome:", func1.get_nome())
+cpf = input ("CPF: ")
+func1.set_cpf(cpf)
+
+print("Salário:", func1.get_salario())
 
 
